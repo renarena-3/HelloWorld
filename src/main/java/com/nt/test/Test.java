00d4a038dcea7706a61bda1ea4ded399cfd6c2e2
renarena-3 <52742284+renarena-3@users.noreply.github.com>
2020-09-19 17:14:07 +0530

    Update Test.java
    this is just now

--- src/main/java/com/nt/test/Test.java
+++ src/main/java/com/nt/test/Test.java
@@ -4,5 +4,7 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("done");
 	System.out.println("this is just now added");
+	System.out.println("this is very latest  added");
+	
 }
 }
